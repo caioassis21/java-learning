4557ccd99df02abdc3f1faede7db0b33a5307b0c
Caio Assis <caioassisgyn@gmail.com>
2025-03-04 13:28:16 -0300

    Adiciona projetos iniciais

---
+++ src/Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Estou aprendendo Java!");
+        System.out.println("Isso é incrível!");
+    }
+}
