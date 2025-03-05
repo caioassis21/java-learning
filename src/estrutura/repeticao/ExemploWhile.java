fac093df29ddfb323e145d1946cd42892f1acf2e
Caio Assis <caioassisgyn@gmail.com>
2025-03-05 15:56:24 -0300

    Exemplo estrutura while

---
+++ src/estrutura/repeticao/ExemploWhile.java
@@ -0,0 +1,17 @@
+package estrutura.repeticao;
+
+/**
+ * Exemplo wile: Contar de 1 ate 50
+ */
+
+public class ExemploWhile {
+    public static void main(String[] args) {
+
+        int cont = 1;
+
+        while (cont <= 50) {
+            System.out.println(cont);
+            cont = cont + 1;
+        }
+    }
+}
